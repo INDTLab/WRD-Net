59515f4d064eabd74962c1c60143c2db1077c62f
fanshu2000 <68736737+fanshu2000@users.noreply.github.com>
2023-12-13 20:38:21 +0800

    Add files via upload

--- test.py
+++ test.py
@@ -25,7 +25,8 @@ from torch import einsum
 from skimage import transform,data
 torch.manual_seed(42)
 
-class Model(nn.Module):
+
+class WRDNet(nn.Module):
     def __init__(self,
             num_stages=4,
             num_path=[4,4,4,4],
@@ -38,7 +39,8 @@ class Model(nn.Module):
             in_channels=3,
             out_channels=20,
             **kwargs,):
-        super(Model,self).__init__()
+
+        super(WRDNet,self).__init__()
         self.in_channels = in_channels
         self.out_channels = out_channels
         self.num_stages = num_stages
@@ -84,7 +86,7 @@ class Model(nn.Module):
                 drop_path_list=dpr[idx],
             ) for idx in range(self.num_stages)
         ])
-        #Decoder_ASP(Atrous Spatial Pyramid)
+        # Decoder_ASP(Atrous Spatial Pyramid)
         self.decoder_patch_embed_stages = nn.ModuleList([
             Patch_Embed_stage(
                 embed_dims[idx],
@@ -92,7 +94,7 @@ class Model(nn.Module):
                 isPool=False,
             ) for idx in range(self.num_stages)
         ])
-        #Decoder_PAViT(Parallel Attention Vision Transformer)
+        # Decoder_PAViT(Parallel Attention Vision Transformer)
         self.decoder_mhca_stages = nn.ModuleList([
             MHCA_stage(
                 embed_dims[idx],
@@ -106,8 +108,8 @@ class Model(nn.Module):
             ) for idx in range(self.num_stages)
         ])
         # Decoder_conv
-        self.decoder_stem = nn.Conv2d(embed_dims[0],out_channels,1)
-
+        self.decoder_stem = nn.Conv2d(embed_dims[0], out_channels, 1)
+        
         self.apply(self._init_weights)
 
     def _init_weights(self, m):
@@ -121,7 +123,7 @@ class Model(nn.Module):
             nn.init.constant_(m.weight, 1.0)
 
     def forward(self,x):
-        # 1. Run the images through our WRD-Net
+        # 1. Run the images through our PAViT
         embed = []
         x = self.stem(x)  # Shape : [B, C, H/4, W/4]
         embed.append(x)
@@ -133,13 +135,13 @@ class Model(nn.Module):
 
         for idx in range(self.num_stages):
             if idx == 0:
-                x = F.interpolate(embed[idx], size=(38, 50), mode='bilinear', align_corners=True)
+                x = F.interpolate(embed[idx], size=(50, 38), mode='bilinear', align_corners=True)
             if idx == 1:
-                x = F.interpolate(x, size=(75, 100), mode='bilinear', align_corners=True)
+                x = F.interpolate(x, size=(100, 75), mode='bilinear', align_corners=True)
             if idx == 2:
-                x = F.interpolate(x, size=(150, 200), mode='bilinear', align_corners=True)
+                x = F.interpolate(x, size=(200, 150), mode='bilinear', align_corners=True)
             if idx == 3:
-                x = F.interpolate(x, size=(300, 400), mode='bilinear', align_corners=True)
+                x = F.interpolate(x, size=(400, 300), mode='bilinear', align_corners=True)
             decoder_att_inputs = self.decoder_patch_embed_stages[self.num_stages - 1 - idx](x)
             x = self.decoder_mhca_stages[self.num_stages - 1 - idx](decoder_att_inputs)
         # 2. Use a 1x1 conv to get one unnormalized heatmap per location
@@ -149,6 +151,39 @@ class Model(nn.Module):
         # 4. Calculate the coordinates
         coords = dsnt(heatmaps)
 
+        return coords, heatmaps
+
+class Model(nn.Module):
+    def __init__(self,
+                 weight,
+                 num_stages=4,
+                 num_path=[3,3,3,3],
+                 num_layers=[1,1,1,1],
+                 embed_dims=[64, 128, 256, 512],
+                 mlp_ratios=[4, 4, 4, 4],
+                 num_heads=[8, 8, 8, 8],
+                 drop_path_rate=0.0,
+                 num_classes=1000,
+                 in_channels=3,
+                 out_channels=20,*args,**kwargs):
+
+        super(Model,self).__init__()
+        self._net = PAViT(num_stages=num_stages,
+            num_path=num_path,
+            num_layers=num_layers,
+            embed_dims=embed_dims,
+            mlp_ratios=mlp_ratios,
+            num_heads=num_heads,
+            drop_path_rate=drop_path_rate,
+            num_classes=num_classes,
+            in_channels=in_channels,
+            out_channels=out_channels).to(device)
+        self._net.load_state_dict(torch.load(weight, map_location=device)['net'])
+        for parameter in self._net.parameters():
+            parameter.requires_grad = False
+
+    def forward(self,x):
+        coords, heatmaps = self._net(x)
         return coords, heatmaps
 
 
@@ -164,9 +199,8 @@ def get_file(img_path,label_path):
     return data_lst
 
 class JointsDataSet(Dataset):
-    def __init__(self,data_lst,data_augment=None):
+    def __init__(self,data_lst):
         self.data_lst = data_lst
-        self.data_augment = data_augment
         print(f'got {len(data_lst)} images and ground-truths')
 
     def __getitem__(self,index):
@@ -175,7 +209,7 @@ class JointsDataSet(Dataset):
         name = self.data_lst[index][2]
         img = TF.to_tensor(img).div(255)
         lab = torch.from_numpy(lab)
-        image_size = [img.shape[2],img.shape[1]]#[400,300]
+        image_size = [img.shape[2], img.shape[1]]  # [400,300]
         lab_tensor = (lab * 2 + 1) / torch.Tensor(image_size) - 1
         if self.data_augment:
             img,lab_tensor = self.data_augment(img,lab_tensor)
@@ -190,7 +224,7 @@ label_root_path = './data/WRSD/annotations'
 
 data_lst = get_file(img_root_path,label_root_path)
 
-device = torch.device("cuda:1" if torch.cuda.is_available() else "cpu")
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
 lr = 5e-4
 weight_decay = 1e-4
@@ -202,25 +236,23 @@ val_mse_lst = []
 
 evaluate_interval = 20
 save_interval = 50
+
+weight = []
+for j in range(3):
+    pweight='./models/'+str(j)+'_model_epoch100.pth'
+    weight.append(pweight)
+
+coords_path = './results/coordsresult/'
+if not os.path.exists(coords_path):
+    os.makedirs(coords_path)
+
 best_val_loss = 100.0
-
-# Save path for models
-snapshot_path = './results/model/'
-# Save path for detection results
-heatmap_path = './results/coordsresult/'
-# Save path for loss arrays
-log_path = './results/loss/'
-if not os.path.exists(snapshot_path):
-    os.makedirs(snapshot_path)
-if not os.path.exists(heatmap_path):
-    os.makedirs(heatmap_path)
-if not os.path.exists(log_path):
-    os.makedirs(log_path)
-
+time_lst = []
 
 kf = KFold(n_splits = 3)
 for j,(train_index,val_index) in enumerate(kf.split(data_lst)):
-    net = Model(num_stages=4,
+    net = Model(weight[j],
+            num_stages=4,
             num_path=[3,3,3,3],
             num_layers=[1,1,1,1],
             embed_dims=[64, 128, 256, 512],
@@ -238,16 +270,18 @@ for j,(train_index,val_index) in enumerate(kf.split(data_lst)):
     val_set = JointsDataSet(val_lst)
     train_loader = DataLoader(train_set, batch_size=train_bs, shuffle=True, num_workers=8, pin_memory=True)
     val_loader = DataLoader(val_set, batch_size=1, shuffle=False, num_workers=1, pin_memory=True)
+    print(f'{j} flod:evaluate model on validation set......')
 
-    for epoch in range(num_epochs):
-        train_mse = 0
-        tic = time.time()
-        net.train()
-        for img,target_var,name in train_loader:
-            optimizer.zero_grad()
+    net.eval()
+    val_mse = 0
+    start = time.time()
+    with torch.no_grad():
+        for img,target_var,name in val_loader:
             img = img.to(device)
             target_var = target_var.to(device)
-            coords, heatmaps = net(img)
+            coords, heatmaps = net(img) 
+            imgsize_tensor = torch.Tensor([300, 400]).to(device)
+            save_coords = ((coords+1) * imgsize_tensor - 1)/2
             # Per-location euclidean losses
             euc_losses = euclidean_losses(coords, target_var)
             # Per-location regularization losses
@@ -255,54 +289,14 @@ for j,(train_index,val_index) in enumerate(kf.split(data_lst)):
             # Per-location focal losses
             focal_losses = focal_reg_losses(heatmaps, target_var, sigma_t=1.0,gamma = 1)
             # Combine losses into an overall loss
-            loss = average_loss(euc_losses + reg_losses + focal_losses)
-            loss.backward()
-            optimizer.step()
-            train_mse += loss.item()*len(img)
-        toc = time.time()
-        mean_loss = train_mse/len(train_set)
-        train_mse_lst.append(mean_loss)
-        np.save(log_path + f'{j}_train_mse.npy',np.array(train_mse_lst))
-
-        print(f'{j} flod:[{epoch+1}]/[{num_epochs}] train loss:{mean_loss} time consumption:{toc-tic}s')
-        if (epoch+1) % evaluate_interval == 0:
-            print(f'{j} flod:evaluate model on validation set......')
-            net.eval()
-            val_mse = 0
-            with torch.no_grad():
-                for img,target_var,name in val_loader:
-                    img = img.to(device)
-                    target_var = target_var.to(device)
-                    coords, heatmaps = net(img)
-                    imgsize_tensor = torch.Tensor([img.shape[3],img.shape[2]]).to(device)#[400,300]
-                    save_coords = ((coords + 1) * imgsize_tensor - 1) / 2
-                    # Per-location euclidean losses
-                    euc_losses = euclidean_losses(coords, target_var)
-                    # Per-location regularization losses
-                    reg_losses = js_reg_losses(heatmaps, target_var, sigma_t=1.0)
-                    # Per-location focal losses
-                    focal_losses = focal_reg_losses(heatmaps, target_var, sigma_t=1.0,gamma = 1)
-                    # Combine losses into an overall loss
-                    val_loss = average_loss(euc_losses + reg_losses + focal_losses)
-                    val_mse += val_loss.item()
-                    # Save results on test set
-                    name = ''.join(name)
-                    if (epoch+1) % num_epochs == 0:
-                        np.save(heatmap_path+f'val_{epoch+1}_{name}.npy',save_coords.cpu().numpy()[0])
-            mean_val_loss = val_mse/len(val_set)
-            val_mse_lst.append(mean_val_loss)
-            np.save(log_path + f'{j}_val_mse.npy',np.array(val_mse_lst))
-
-            
-            print(f'{j} flod:loss on val set{mean_val_loss}')
-            # Save best model on test set
-            if mean_val_loss < best_val_loss:
-                best_val_loss = mean_val_loss
-                state_dict = {'net':net.state_dict(),'optimizer':optimizer.state_dict(),'epoch':epoch}
-                torch.save(state_dict, snapshot_path + f'{j}_best_model.pth')
-                print(f'{j} flod:save best val model at epoch{epoch+1}')
-
-        if (epoch+1) % save_interval == 0:
-            # Save the model every save_interval
-            state_dict = {'net':net.state_dict(),'optimizer':optimizer.state_dict(),'epoch':epoch}
-            torch.save(state_dict, snapshot_path + f'{j}_model_epoch{epoch+1}.pth')
+            val_loss = average_loss(euc_losses + reg_losses + focal_losses)
+            val_mse += val_loss.item()
+            # Save results on test set
+            name = ''.join(name)
+            np.save(coords_path+f'val_{100}_{name}.npy',save_coords.cpu().numpy()[0])
+    end = time.time()
+    time_lst.append(end-start)
+    print("推理时间:",time_lst)
+    mean_val_loss = val_mse/len(val_set)
+    val_mse_lst.append(mean_val_loss)
+    print(f'{j} flod:loss on val set{mean_val_loss}')
